311f4f8f441489789292e0346d3a57668c3dc5d7
Gopal-001 <64918520+Gopal-001@users.noreply.github.com>
2020-10-08 13:25:53 +0530

    Update 1.cpp

--- 1.cpp
+++ 1.cpp
@@ -4,9 +4,10 @@ int main()
 {
     string s="animesh";
     cout<<s.length()<<endl;
-    cout<<s.find('m');
-}
-int main()
-{
-    
+    int n = s.length();
+    int cnt=0;
+    for(int i=0;i<n;i++){
+        cnt += s[i];  
+        
+    cout<<"The value of your name : "<<cnt;
 }
